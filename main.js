00ea19795c6d653266aede7bf8b5f108148bb8b5
Mudit_Chandak7C <96127224+Mudit-Alt@users.noreply.github.com>
2022-11-09 17:36:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+video=""
+Status=""
+suii=""
+
 function setup(){
     canvas = createCanvas(440, 350);
     canvas.center();
+    video = createCapture();
+    video.size(440, 350);
+    video.hide();
 }
 
-image
+function draw(){
+    image(video, 0, 0, 440, 350);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById('status').innerHTML = "Status : Detecting Objects";
+    suii = document.getElementById("input_1").value
+}
+
+function modelLoaded(){
+    console.log("Yayyy You Did It !!!!");
+    Status = true;
+}
